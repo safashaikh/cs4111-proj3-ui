d5c7025b298a223c367199324f607a82a8659e62
Bharathi <bharathimurugan@gmail.com>
2021-11-09 20:45:52 -0500

    Added production url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,6 @@ export class AppComponent {
 
 @Injectable()
 export class Globals {
-  hosturl: string = 'http://127.0.0.1:8111';
+  hosturl: string =  'http://127.0.0.1:8111';
+  // hosturl: string = 'http://35.237.138.209:8111'; // Use this during build and deploy  time
 }
